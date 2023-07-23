b47c3e6cd6c3f934e698cfe66d3ec013b900ce73
madargani <mad3913@gmail.com>
2023-07-23 22:01:16 +0800

    July 23

---
+++ 07/23 Given a linked list of 0s, 1s and 2s, sort it/code.py
@@ -0,0 +1,18 @@
+class Solution:
+    def segregate(self, head):
+        freqs = [0] * 3
+        
+        cur = head
+        while cur:
+            freqs[cur.data] += 1
+            cur = cur.next
+        
+        cur = head
+        i = 0
+        while cur:
+            while freqs[i] <= 0:
+                i += 1
+            cur.data = i
+            freqs[i] -= 1
+            
+        return head
