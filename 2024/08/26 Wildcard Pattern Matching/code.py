d5d2c1bdf109086ffeb469811870fa1e4816cfef
madargani <mad3913@gmail.com>
2024-08-28 11:40:38 -0700

    8/26 wildcard

--- 2024/08/26 Wildcard Pattern Matching/code.py
+++ 2024/08/26 Wildcard Pattern Matching/code.py
@@ -1,21 +1,30 @@
 class Solution:
     def wildCard(self, pattern, string):
-        plen, slen = len(pattern), len(string)
-        dp = [[False] * slen for _ in range(plen)] # [pi][si]
+        string = '#' + string
+        dp = [[False] * (len(string) + 1) for _ in range(len(pattern) + 1)] # [patternIndex][stringIndex]
+        dp[0][1] = True
 
-        for i in range(plen):
-            for j in range(slen):
-                if pattern[i] == '*':
-                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
-                elif pattern[i] == '?':
-                    dp[i][j] = dp[i - 1][j - 1]
+        for pi, pc in enumerate(pattern, start=1):
+            for si, sc in enumerate(string, start=1):
+                if pc == '*':
+                    dp[pi][si] = dp[pi - 1][si - 1] or dp[pi - 1][si] or dp[pi][si - 1]
+                elif pc == '?':
+                    dp[pi][si] = dp[pi - 1][si - 1]
                 else:
-                    dp[i][j] = pattern[i] == pattern[j] and dp[i - 1][j - 1]
+                    dp[pi][si] = dp[pi - 1][si - 1] and pc == sc
 
         return dp[-1][-1]
 
 if __name__ == "__main__":
-    ps = []
-    ss = []
+    ps = ['?*?', '*b*']
+    ss = ['a', 'b']
     for p, s in zip(ps, ss):
         print(Solution().wildCard(p, s))
+
+'''
+ #a
+#10
+?01
+*11
+?11
+'''
